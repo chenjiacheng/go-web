e92f7c7a72ce4147f333936a63806bf004dd4fae
chenjiacheng <405339556@qq.com>
2022-12-13 14:31:48 +0800

    显示话题

--- app/models/topic/topic_util.go
+++ app/models/topic/topic_util.go
@@ -2,14 +2,15 @@ package topic
 
 import (
 	"go-web/pkg/app"
-    "go-web/pkg/database"
-    "go-web/pkg/paginator"
+	"go-web/pkg/database"
+	"go-web/pkg/paginator"
+	"gorm.io/gorm/clause"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func Get(idstr string) (topic Topic) {
-	database.DB.Where("id", idstr).First(&topic)
+	database.DB.Preload(clause.Associations).Where("id", idstr).First(&topic)
 	return
 }
 
